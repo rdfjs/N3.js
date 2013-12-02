4fafa77ff585429aff36083579516a1f51ccf94d
Ruben Verborgh <ruben@verborgh.org>
2013-12-02 13:52:13 +0000

    Expose submodules in a browserify-compatible way.
    Closes #14.

--- N3.js
+++ N3.js
@@ -1,20 +1,26 @@
-// Expose the following N3 submodules lazily
-[
-  'Lexer',
-  'Parser',
-  'Writer',
-  'Store',
-  'StreamParser',
-  'StreamWriter',
-  'Util',
-]
-.forEach(function (submodule) {
+// Replace local require by a lazy loader
+var globalRequire = require;
+require = function () {};
+
+// Expose submodules
+var exports = module.exports = {
+  Lexer:        require('./lib/N3Lexer'),
+  Parser:       require('./lib/N3Parser'),
+  Writer:       require('./lib/N3Writer'),
+  Store:        require('./lib/N3Store'),
+  StreamParser: require('./lib/N3StreamParser'),
+  StreamWriter: require('./lib/N3StreamWriter'),
+  Util:         require('./lib/N3Util'),
+};
+
+// Load submodules on first access
+Object.keys(exports).forEach(function (submodule) {
   Object.defineProperty(exports, submodule, {
     configurable: true,
     enumerable: true,
     get: function () {
       delete exports[submodule];
-      return exports[submodule] = require('./lib/N3' + submodule);
+      return exports[submodule] = globalRequire('./lib/N3' + submodule);
     },
   });
 });
