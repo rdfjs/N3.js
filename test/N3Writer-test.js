6cfd0ccf2ae30597c2427ea2e1d4f6d67df44950
Ruben Verborgh <ruben@verborgh.org>
2014-12-28 16:14:07 +0100

    Enhance performance by minimizing _write calls.

--- test/N3Writer-test.js
+++ test/N3Writer-test.js
@@ -96,12 +96,16 @@ describe('N3Parser', function () {
                       '_:a <b> _:c.\n'));
 
     it('should not serialize a literal in the subject',
-      shouldNotSerialize([['"a"', 'b', '"c']],
+      shouldNotSerialize([['"a"', 'b', '"c"']],
                           'A literal as subject is not allowed: "a"'));
 
     it('should not serialize a literal in the predicate',
-      shouldNotSerialize([['a', '"b"', '"c']],
+      shouldNotSerialize([['a', '"b"', '"c"']],
                           'A literal as predicate is not allowed: "b"'));
+
+    it('should not serialize an invalid object literal',
+      shouldNotSerialize([['a', 'b', '"c']],
+                          'Invalid literal: "c'));
 
     it('should not leave leading whitespace if the prefix set is empty',
       shouldSerialize({},
