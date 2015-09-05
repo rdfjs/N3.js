6bd86735696307bbe1130f1b6ec8a30ae4d245a7
Ruben Verborgh <ruben@verborgh.org>
2015-09-05 09:26:53 +0200

    Use base in perf tests.

--- perf/N3Parser-perf.js
+++ perf/N3Parser-perf.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 var N3 = require('../N3');
 var fs = require('fs'),
+    path = require('path'),
     assert = require('assert');
 
 if (process.argv.length !== 3)
   return console.error('Usage: N3Parser-perf.js filename');
 
-var filename = process.argv[2];
+var filename = path.resolve(process.cwd(), process.argv[2]),
+    base = 'file://' + filename;
 
 var TEST = '- Parsing file ' + filename;
 console.time(TEST);
 
 var count = 0;
-new N3.Parser().parse(fs.createReadStream(filename), function (error, triple) {
+new N3.Parser({ documentIRI: base }).parse(fs.createReadStream(filename), function (error, triple) {
   assert(!error, error);
-  if (triple) {
+  if (triple)
     count++;
-  }
   else {
     console.timeEnd(TEST);
     console.log('* Triples parsed: ' + count);
