4bdc83654a8f1c7499b4ab318c32286269b846d7
Iddan Aaronsohn <mail@aniddan.com>
2019-01-26 01:51:46 +0200

    Added remove event for store stream

--- test/N3Store-test.js
+++ test/N3Store-test.js
@@ -1068,6 +1068,34 @@ describe('N3Store', function () {
         new Quad('http://example.com/1', 'http://example.com/2', 'http://example.com/3', new DefaultGraph()),
       ]);
     });
+
+    it('should emit quad every time a quad is removed from the store', function () {
+      var stream = store.match();
+      var removed = [];
+      stream.on('remove', (quad) => {
+        removed.push(quad);
+      });
+      store.addQuad('null', 'null', 'null', 'null');
+      store.removeQuad('null', 'null', 'null', 'null');
+      removed.should.deep.equal([
+        new Quad('null', 'null', 'null', 'null'),
+      ]);
+    });
+
+    it('should emit quad every time a matching quad is removed from the store', function () {
+      var stream = store.match('http://example.com/1');
+      var removed = [];
+      stream.on('remove', (quad) => {
+        removed.push(quad);
+      });
+      store.addQuad('null', 'null', 'null', 'null');
+      store.addQuad('http://example.com/1', 'http://example.com/2', 'http://example.com/3', new DefaultGraph());
+      store.removeQuad('null', 'null', 'null', 'null');
+      store.removeQuad('http://example.com/1', 'http://example.com/2', 'http://example.com/3', new DefaultGraph());
+      removed.should.deep.equal([
+        new Quad('http://example.com/1', 'http://example.com/2', 'http://example.com/3', new DefaultGraph()),
+      ]);
+    });
   });
 });
 
