254bffbb6cde4e924987c8ec547e839e11b571bb
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-03-31 11:54:54 +1100

    chore: automatically detect media type for spec tests

--- spec/parser.js
+++ spec/parser.js
@@ -1,10 +1,14 @@
 const { StreamParser } = require('..');
+const { testCaseToMediaMappings } = require('rdf-test-suite'); 
 
 // Implements the IParser interface from rdf-test-suite
 // https://github.com/rubensworks/rdf-test-suite.js/blob/master/lib/testcase/rdfsyntax/IParser.ts
 module.exports = {
-  parse: function (data, baseIRI, options) {
+  parse: function (data, baseIRI, options, test) {
     return require('arrayify-stream')(require('streamify-string')(data).pipe(
-      new StreamParser(Object.assign({ baseIRI: baseIRI }, options))));
+      new StreamParser(Object.assign({ baseIRI: baseIRI }, {
+        format: test.types && testCaseToMediaMappings[test.types.find(type => type in testCaseToMediaMappings)],
+        ...options
+      }))));
   },
 };
