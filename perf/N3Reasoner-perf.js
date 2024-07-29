06efe633638c3d7b76a5cf9bece58b4d07c37b60
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-07-29 10:40:15 +1000

    chore: update perf script

--- perf/N3Reasoner-perf.js
+++ perf/N3Reasoner-perf.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-const path = require('path');
+const { getTimblAndFoaf, generateDeepTaxonomy, getRdfs } = require('deep-taxonomy-benchmark');
 const N3 = require('..');
-const { load, SUBCLASS_RULE, RDFS_RULE, generateDeepTaxonomy } = require('../test/util');
+
+const SUBCLASS_RULE = N3.getRulesFromDataset(new N3.Store((new N3.Parser({ format: 'text/n3' })).parse('{ ?s a ?o . ?o <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?o2 . } => { ?s a ?o2 . } .')));
 
 async function deepTaxonomy(extended = false) {
-  for (let i = 1; i <= 6; i++) {
+  for (let i = 1; i <= (extended ? 3 : 5); i++) {
     const TITLE = `test-dl-${10 ** i}.n3`;
     const store = generateDeepTaxonomy(10 ** i, extended);
 
@@ -15,14 +16,8 @@ async function deepTaxonomy(extended = false) {
 }
 
 async function run() {
-  const store = new N3.Store();
-  console.time('loading foaf ontology');
-  await load(path.join(__dirname, './data/foaf.ttl'), store);
-  console.timeEnd('loading foaf ontology');
-
-  console.time('loading tim berners lee profile card');
-  await load(path.join(__dirname, './data/timbl.ttl'), store);
-  console.timeEnd('loading tim berners lee profile card');
+  const RDFS_RULE = N3.getRulesFromDataset(await getRdfs());
+  const store = new N3.Store([...await getTimblAndFoaf()]);
 
   console.time('Reasoning');
   new N3.Reasoner(store).reason(RDFS_RULE);
