3aaeabce35acc1a570538f92b087cf3ace19bd2e
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-08-26 11:28:11 +1000

    chore(deps): update arrayify stream (#426)
    * chore(deps): update arrayify stream
    
    * chore: convert arrayify-stream default export to named export

--- spec/parser.js
+++ spec/parser.js
@@ -4,7 +4,7 @@ const { StreamParser } = require('..');
 // https://github.com/rubensworks/rdf-test-suite.js/blob/master/lib/testcase/rdfsyntax/IParser.ts
 module.exports = {
   parse: function (data, baseIRI, options) {
-    return require('arrayify-stream')(require('streamify-string')(data).pipe(
+    return require('arrayify-stream').arrayifyStream(require('streamify-string')(data).pipe(
       new StreamParser(Object.assign({ baseIRI: baseIRI }, options))));
   },
 };
