86a388edda8eb3f8368cd6fe1d8ec45c124f30d4
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-09-17 18:47:55 +1000

    chore: add data generator file (#442)
    * chore: add data generator file
    
    * chore: fix lint errors
    
    * Update .gitignore

---
+++ perf/data/trigGen.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const N3 = require('../../lib');
+
+const { quad, namedNode, literal } = N3.DataFactory;
+const dim = 30;
+
+const writer = new N3.StreamWriter();
+
+// eslint-disable-next-line
+fs.rmSync(path.join(__dirname, 'data.trig'), { force: true });
+const outputStream = fs.createWriteStream(path.join(__dirname, 'data.trig'));
+writer.pipe(outputStream);
+
+const prefix = 'http://example.org/#';
+for (let i = 0; i < dim; i++) {
+  for (let j = 0; j < dim; j++) {
+    for (let k = 0; k < dim; k++) {
+      for (let l = 0; l < dim; l++) {
+        writer.write(quad(
+          namedNode(prefix + i), namedNode(prefix + j), (k % 2 === 0 ? namedNode : literal)(prefix + k), namedNode(prefix + l),
+        ));
+      }
+    }
+  }
+}
+
+writer.end();
